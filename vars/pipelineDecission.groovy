330b1008205c61dca6482f56ec0f20215ddebc8c
jai <jk8142727101@gmail.com>
2024-11-30 19:38:26 +0530

    jenkins-library

---
+++ vars/pipelineDecission.groovy
@@ -0,0 +1,18 @@
+
+#!groovy
+
+//declaring a function
+def decidePipeline(Map configMap) {
+    type = configMap.get("type")
+    switch(type) {
+        case "nodejsEKS":
+            nodejsEKS(configMap)
+            break
+        case "nodejsVM":
+            nodejsVM(configMap)
+            break
+        default:
+            error "type is not matched" 
+            break
+    }
+}
---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,9 @@
+static final String region(){
+    return "us-east-1"
+}
+static final String nexusURL(){
+    return "nexus.jai-awsdevops.online:8081"
+}
+static final String account_id(){
+    return "315069654700"
+}
